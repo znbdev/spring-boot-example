6a477e84793a80a8005ac005fa7f1e1685f6a8be
znbdev <znbdev@outlook.com>
2024-05-17 11:32:38 +0900

    Modify

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(properties = {"info.env=test"})
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Value("${info.env}")
+    private String env;
 
+    @Test
+    void contextLoads() throws Exception {
+        System.out.println("UT env: " + env);
+        assertEquals("test", env);
+    }
 }
