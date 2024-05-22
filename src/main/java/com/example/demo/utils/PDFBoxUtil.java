188691ec337e344890c21aee08c4ecd35ef60993
znbdev <znbdev@outlook.com>
2024-05-23 01:32:25 +0900

    Modify

--- src/main/java/com/example/demo/utils/PDFBoxUtil.java
+++ src/main/java/com/example/demo/utils/PDFBoxUtil.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,7 +22,7 @@ import java.io.IOException;
 public class PDFBoxUtil {
     private final AppProperties appProperties;
 
-    public void createPdf() throws IOException {
+    public void createPdf(String pdfName) throws IOException {
         // 创建 PDF 文档
         PDDocument document = new PDDocument();
         PDPage page = new PDPage();
@@ -42,25 +43,62 @@ public class PDFBoxUtil {
         contentStream.showText("Hello, PDFBox!");
         contentStream.endText();
 
-        // 添加印章图像到页面
-        String imagePath = appProperties.getTmpDir() + File.separator + "zhang.png";
-        PDImageXObject image = PDImageXObject.createFromFile(imagePath, document);
-        float imageWidth = 30; // 图像宽度
-        float imageHeight = 30; // 图像高度
-        float imageX = pageSize.getWidth() - imageWidth - 100; // 图像 X 坐标
-        float imageY = pageSize.getHeight() - imageHeight - 100; // 图像 Y 坐标
-        contentStream.drawImage(image, imageX, imageY, imageWidth, imageHeight);
-
         // 关闭页面内容流
         contentStream.close();
 
         // 保存 PDF 文件
-        String pdfFilePath = appProperties.getTmpDir() + File.separator + "pdfbox_example_with_seal.pdf";
+        String pdfFilePath = appProperties.getTmpDir() + File.separator + pdfName + ".pdf";
         document.save(new File(pdfFilePath));
         document.close();
 
         log.info("PDF 文件已创建成功！你可以在[{}]查看", pdfFilePath);
     }
 
+    public void fillPdfWithVariables(String sourcePdfFilePath, String destinationPdfFilePath, Map<String, String> variableMap, String imagePath) throws IOException {
+        try (PDDocument document = PDDocument.load(new File(sourcePdfFilePath))) {
+            PDPage page = document.getPage(0); // 获取第一页
+
+            // 获取页面尺寸
+            PDRectangle pageSize = page.getMediaBox();
+
+            // 创建页面内容流
+            PDPageContentStream contentStream = new PDPageContentStream(document, page, AppendMode.APPEND, true);
+
+            // 设置字体和字号
+            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+
+
+            float textX = pageSize.getWidth() - 500;
+            float textY = pageSize.getHeight() - 300;
+
+            int i = 1;
+            // 填写变量信息到页面
+            for (Map.Entry<String, String> entry : variableMap.entrySet()) {
+                String variableName = entry.getKey();
+                String variableValue = entry.getValue();
+                contentStream.beginText();
+                float y = textY - 30 * i;
+                log.info("variableName:{}, variableValue:{}, x:{}, y:{}", variableName, variableValue, textX, y);
+                contentStream.newLineAtOffset(textX, y); // 修改为合适的坐标
+                contentStream.showText(variableValue);
+                contentStream.endText();
+                i++;
+            }
+
+            // 添加印章图像到页面
+            PDImageXObject image = PDImageXObject.createFromFile(imagePath, document);
+            float imageWidth = 30; // 图像宽度
+            float imageHeight = 30; // 图像高度
+            float imageX = pageSize.getWidth() - 100; // 图像 X 坐标
+            float imageY = pageSize.getHeight() - 400; // 图像 Y 坐标
+            contentStream.drawImage(image, imageX, imageY, imageWidth, imageHeight);
+            // 关闭页面内容流
+            contentStream.close();
+
+            // 保存填充后的 PDF 文件
+            document.save(new File(destinationPdfFilePath));
+            log.info("PDF 文件已创建成功！{}", destinationPdfFilePath);
+        }
+    }
 
 }
--- src/test/java/com/example/demo/utils/PDFBoxUtilTest.java
+++ src/test/java/com/example/demo/utils/PDFBoxUtilTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
@@ -35,7 +37,19 @@ class PDFBoxUtilTest {
     @Test
     void createPdfTest() throws IOException {
         PDFBoxUtil pdfBoxUtil = new PDFBoxUtil(appProperties);
-        pdfBoxUtil.createPdf();
+        pdfBoxUtil.createPdf("test");
+        assertTrue(true);
+    }
+
+    @Test
+    void fillPdfWithVariablesTest() throws IOException {
+        String pdfFilePath = appProperties.getTmpDir() + File.separator + "test" + ".pdf";
+        String destinationPdfFilePath = appProperties.getTmpDir() + File.separator + "test2.pdf";
+        Map<String, String> variableMap = Map.of("name", "nnn", "age", "25", "address", "aaa");
+        String imagePath = appProperties.getTmpDir() + File.separator + "zhang.png";
+
+        PDFBoxUtil pdfBoxUtil = new PDFBoxUtil(appProperties);
+        pdfBoxUtil.fillPdfWithVariables(pdfFilePath, destinationPdfFilePath, variableMap, imagePath);
         assertTrue(true);
     }
 }
